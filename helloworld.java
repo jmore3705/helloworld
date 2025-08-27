67c60a7897938a69d329db28425cf68c88ba59c3
jmore3705 <jmore3705@gmail.com>
2025-08-27 16:36:37 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch40 team with Isar , Noman Amitabh");
-      System.out.println("Hello Batch batch40 WITH FOR STUDENTS RUNNING in front of me and Myusr, Rohit, 3rd Commit Aditya in making CI automated vikas teach devops CI proecss");
+      System.out.println("Hello I am Sagar More doing jenkins practical on 27th August 2025 on occasion of Ganesh Chaturthi");
     }  
 } 
